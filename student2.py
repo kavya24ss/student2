469efc55d47ab6a2663240f64677234300541d04
kavya24ss <sagikavyasri@gmail.com>
2025-11-22 11:07:12 +0530

    Update student2.py

--- student2.py
+++ student2.py
@@ -2,7 +2,7 @@ import sys
 
 if len(sys.argv) == 3:
     script_name = sys.argv[0]
-    name = sys.agrv[1]
+    name = sys.argv[1]
     rollno = sys.argv[2]
     print("user provided input values:")
 else:
